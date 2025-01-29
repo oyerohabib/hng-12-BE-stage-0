1bcb52c563aa39730e6abb4e43e73dc2b4e5c8a2
oyerohabib <oyerohabib@yahoo.com>
2025-01-29 14:23:15 +0100

    chore: fixed build error

---
+++ dist/index.js
@@ -0,0 +1,24 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
+const app = (0, express_1.default)();
+const PORT = process.env.PORT || 3000;
+// Enable CORS for all routes
+app.use((0, cors_1.default)());
+// Main endpoint
+app.get("/", (req, res) => {
+    const response = {
+        email: "oyerohabib@yahoo.com",
+        current_datetime: new Date().toISOString(),
+        github_url: "https://github.com/oyerohabib/hng-12-BE-stage-0",
+    };
+    res.status(200).json(response);
+});
+// Start server
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
